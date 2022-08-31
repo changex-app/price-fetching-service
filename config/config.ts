79f1f4c9268cc4c11588eae431d5877e96170e51
Nikolay Chankov <nikolay.chankov@gostartups.net>
2022-08-31 16:06:45 +0300

    config variable for database changed

--- config/config.ts
+++ config/config.ts
@@ -3,12 +3,12 @@ const env = process.env.NODE_ENV || 'development';
 const config = {
         development: {
             port: process.env.PORT || 8081,
-            dbURL: process.env.DATABASE_API_URL,
+            dbURL: process.env.DATABASE_URL,
             origin: ['http://localhost:5555', 'http://localhost:4200'],
         },
         production: {
             port: process.env.PORT || 8080,
-            dbURL: process.env.DATABASE_API_URL,
+            dbURL: process.env.DATABASE_URL,
             origin: "*",
         }
 }
