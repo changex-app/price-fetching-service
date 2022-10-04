87d0eec87421c24b1311219bcc469be78d464256
Nikolay Chankov <nikolay.chankov@gostartups.net>
2022-10-04 21:00:07 +0300

    removed replace of url encoding

--- services/coins.market.service.ts
+++ services/coins.market.service.ts
@@ -101,7 +101,7 @@ export async function requestMarketsCoingeckoData(coins: any): Promise<any> {
         let url = `${process.env.COINGECKO_API_URL}coins/markets?ids=${coins.join('%2C')}&vs_currency=${exchange_rate}`;
 
         //Replace is done because sometimes " ` " is replaced with encoding %27,
-        url.replace('%27,', '');
+        let encodedURL = url.replace('%27,', '');
 
         try {
            await axios.get(url)
